220c7fd98f717f1673f852f5101784889024816c
Hetul Patel <patehetu239@gmail.com>
2024-12-23 00:56:54 -0500

    initial implementation of sparsity detection. Incomplete commit before going to sleep

--- include/sparse_operations.hpp
+++ include/sparse_operations.hpp
@@ -2,9 +2,14 @@
 #define SPARSE_OPERATIONS_HPP
 
 #include <xtensor/xarray.hpp>
+#include <xtensor/xtensor.hpp>
 namespace sparse_ops {
-    // Function for densor tensor addition as an initial test
-    xt::xarray<double> addTensor(const xt::xarray<double>& tensor1, const xt::xarray<double>& tensor2);
+    // operations to check if a tensor/array is sparse based on threshold, and operation to return sparsity percentage
+    template <typename Tensor>
+    bool isSparse(const Tensor& tensor, double threshold=0.8);
+
+    template <typename Tensor>
+    double sparsity(const Tensor& tensor);
 }
 
 #endif // SPARSE_OPERATIONS_HPP
--- src/sparse_operations.cpp
+++ src/sparse_operations.cpp
@@ -1,10 +1,40 @@
 #include "sparse_operations.hpp"
+#include <algorithm>
 
 namespace sparse_ops {
-    xt::xarray<double> addTensor(const xt::xarray<double>& tensor1, const xt::xarray<double>& tensor2) {
-        if(tensor1.shape() != tensor2.shape()) {
-            throw std::invalid_argument("Tensors must have the same shape for addition operation");
-        }
-        return tensor1 + tensor2;
+    template <typename Tensor>
+    bool isSparse(const Tensor& tensor, double threshold) {
+        /*
+        Two approaches:
+        1. xt::equal and xt::sum are highly optimized in xtensor making it faster than a linear scan,
+           however, it creates a temporary tensor which can be memory intensive
+        2. Linear scan with early stopping doesn't create a temporary tensor but is slower
+        */
+       // Approach 1
+       auto zero_count = xt::sum(xt::equal(tensor, 0.0));
+       double sparsity = static_cast<double>(zero_count()) / tensor.size();
+       return sparsity >= threshold;
+       // Approach 2
+       /*
+       size_t zero_count = 0;
+       size_t max_zeros = static_cast<size_t>(tensor.size() * threshold);
+         for (const auto& val : tensor) {
+            if (val == 0.0) {
+                zero_count++;
+                if(zero_count >= max_zeros) {
+                    return true;
+                }
+            }
+         }
+         return false;
+       */
     }
+    template <typename Tensor>
+    double sparsity(const Tensor& tensor) {
+       auto zero_count = xt::sum(xt::equal(tensor, 0.0));
+       return static_cast<double>(zero_count()) / tensor.size();
+    }
+
+    template bool isSparse<xt::xarray<double>>(const xt::xarray<double>&, double);
+    template bool isSparse<xt::xtensor<double, xt::dynamic_shape>>(const xt::xtensor<double, xt::dynamic_shape>&, double);
 }
